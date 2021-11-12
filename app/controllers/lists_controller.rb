bdaea20bdc782b1a793d162772c44b2ad057bf56
Giuseppe Russo <russo.giu@outlook.com>
2021-11-12 11:29:05 +0000

    watch list done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,35 @@
+class ListsController < ApplicationController
+  before_action :find_list, only: [:show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+List.create( name: 'Horror')
+List.create( name: 'Thriller')
+List.create( name: 'Comedy')
+List.create( name: 'Sci-Fi')
+List.create( name: 'Animation')
+List.create( name: 'Western')
+
+
+require 'json'
+require 'open-uri'
+# instead of doing db:drop - this will clear data before creating again
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)['results']
+poster_url = "https://image.tmdb.org/t/p/w500"
+
+movies.each do |movie|
+  puts "creating #{movie['title']}"
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: poster_url + movie['poster_path'],
+    rating: movie['vote_average']
+  )
+end

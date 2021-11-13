e02c98df3921538fc64e7b432f4c3b99190ffba6
Giuseppe Russo <russo.giu@outlook.com>
2021-11-13 18:33:03 +0000

    kind of good scss done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -25,6 +25,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:movie_id, :comment)
+    params.require(:bookmark).permit(:movie_id, :comment, :photo)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-List.create( name: 'Horror')
-List.create( name: 'Thriller')
-List.create( name: 'Comedy')
-List.create( name: 'Sci-Fi')
-List.create( name: 'Animation')
-List.create( name: 'Western')
-
-
 require 'json'
 require 'open-uri'
 # instead of doing db:drop - this will clear data before creating again
